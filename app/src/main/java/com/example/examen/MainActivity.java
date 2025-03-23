953ae1b4f99ad4d97bfb3a791061441b86b31610
16Adonis <cgurumendic@uteq.edu.ec>
2025-03-23 18:07:45 -0500

    actualizado

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -25,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         listaProductos = new ArrayList<>();
-        listaProductos.add(new Producto("Camiseta Blanca", "$11.99", R.drawable.camiseta));
+        listaProductos.add(new Producto("Camiseta Blanca", "$10.99", R.drawable.camiseta));
         listaProductos.add(new Producto("Jeans Azul", "$30.50", R.drawable.jeans));
         listaProductos.add(new Producto("Zapatillas DragonBall", "$25.00", R.drawable.zapatillas));
-        listaProductos.add(new Producto("Camiseta Blanca", "$12.99", R.drawable.camiseta));
-        listaProductos.add(new Producto("Jeans Azul", "$24.50", R.drawable.jeans));
-        listaProductos.add(new Producto("Zapatillas Negras", "$39.99", R.drawable.zapatillas));listaProductos.add(new Producto("Camiseta Blanca", "$12.99", R.drawable.camiseta));
-        listaProductos.add(new Producto("Jeans Azul", "$24.50", R.drawable.jeans));
-        listaProductos.add(new Producto("Zapatillas Negras", "$39.99", R.drawable.zapatillas));
+        listaProductos.add(new Producto("Gorra", "$20.00", R.drawable.gorra));
+        listaProductos.add(new Producto("Camiseta Overzide", "$20.50", R.drawable.camisetaoverzice));
+        listaProductos.add(new Producto("Jeans Clasicos", "$49.99", R.drawable.jeansclasicos));
+        listaProductos.add(new Producto("Medias", "$7.50", R.drawable.medias));
+        listaProductos.add(new Producto("Abrigo", "$19.99", R.drawable.abrigo));
 
         ProductoAdapter adapter = new ProductoAdapter(this, listaProductos);
         recyclerView.setAdapter(adapter);
